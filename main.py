5b04917ba04a5da71b0d46f6cd47ffbeb855ba53
Jadon <47489511+gonhunterx@users.noreply.github.com>
2023-09-29 23:37:29 -0400

    interface

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
-# creating a game where functions open a file that will save progress. 
-# you can create a file. and name it yourself. 
+# creating a game where functions open a file that will save progress.
+# you can create a file. and name it yourself.
 # the file can essentially be "Player" the class
-# and as you prog you gain stats and you can start new if you want. 
-# functions can be places basically that you enter where the file opens in a with statement 
-# easy story to start off with nothing too serious. 
+# and as you prog you gain stats and you can start new if you want.
+# functions can be places basically that you enter where the file opens in a with statement
+# easy story to start off with nothing too serious.
+import customtkinter
 
-class Player:
-    def __init__(self, name):
-        self.name = name
-        self.coins = 0
-        self.health = 0
+customtkinter.set_appearance_mode("light")
+customtkinter.set_default_color_theme("dark-blue")
 
-def get_player_info():
-    with open()
-        print("Welcome to the game")
-        print("What is your name?")
-        player_name = input("Enter name: ")
+root = customtkinter.CTk()
+root.geometry("500x350")
 
 
-
-def menu():
-    
-
+def login():
+    print("Test")
 
 
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
 
+label = customtkinter.CTkLabel(master=frame, text="Login System")
+label.pack(pady=12, padx=10)
 
-def main():
-    menu()
-    pass
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
+entry1.pack(pady=12, padx=10)
 
-if __name__ == "__main__":
-    main()
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
+entry2.pack(pady=12, padx=10)
+
+button = customtkinter.CTkButton(master=frame, text="Login", command=login)
+button.pack(pady=12, padx=10)
+
+checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember Me")
+checkbox.pack(pady=12, padx=10)
+
+root.mainloop()
